61d8c73949a1a6a870a5794a136aa4dc018c2e07
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-02-23 21:33:05 +0500

    hero added

---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,24 @@
+function Hero() {
+  return (
+    <div className="relative w-full h-screen flex text-white text-center">
+      <div className="absolute inset-0 bg-gradient-to-r from-[#071629]/100 z-10" />
+
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('/kaaba-lg.jpg')" }}
+      />
+
+      <div className="z-10 max-w-2xl px-6 lg:pt-60 pt-36">
+        <h1 className="text-4xl md:text-5xl font-bold text-left">
+          We believe in Islamic Finance & Qarz-e-Hasana !
+        </h1>
+        <p className="mt-4 text-sm text-left text-slate-300">
+          A platform for managing Qarz-e-Hasana, ensuring transparency,
+          fairness, and ease in loans.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,39 +1,44 @@
-import React from "react";
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 function Navbar() {
+  useState()
   return (
-    <nav className="bg-[#081b33] text-white h-16 flex flex-nowrap items-center justify-center">
-      <div className="flex w-5/6  items-center justify-between">
-        <div>
-          <Image
-            src={"/logo.png"}
-            alt={"Islamic Qarz"}
-            width={80}
-            height={80}
-          />
-        </div>
-        <ul className="flex items-center justify-between w-auto gap-4 text-sm">
-          <Link href={"/explore-loan-options"}>
-            <li>Explore</li>
-          </Link>
-          <Link href={"/about"}>
-            <li>About</li>
-          </Link>
-          <Link href={"/contact"}>
-            <li>Contact</li>
-          </Link>
-          <li>
-            <Link href={"/login"}>
-              <button className="bg-white text-[#081b33] p-2 border-2 rounded-2xl ml-8 px-4 font-bold">
-                Login
-              </button>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <div className="h-16 bg-[#071629]" />
+      <nav className="text-white w-full h-16 flex flex-nowrap items-center justify-center fixed top-0 backdrop-blur z-50 shadow-md shadow-[#071629]">
+        <div className="flex w-5/6 items-center justify-between">
+          <div>
+            <Image
+              src={"/logo.png"}
+              alt={"Islamic Qarz"}
+              width={80}
+              height={80}
+            />
+          </div>
+            <ul className="hidden items-center justify-between w-auto gap-4 text-sm  md:flex">
+              <Link href={"/explore-loan-options"}>
+                <li>Explore</li>
+              </Link>
+              <Link href={"/about"}>
+                <li>About</li>
+              </Link>
+              <Link href={"/contact"}>
+                <li>Contact</li>
+              </Link>
+              <li>
+                <Link href={"/login"}>
+                  <button className="bg-white text-[#081b33] p-2 border-2 rounded-2xl ml-8 px-4 font-bold">
+                    Login
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+      </nav>
+    </>
   );
 }
 

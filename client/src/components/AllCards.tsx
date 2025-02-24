1312deac9afe9b922e4e7f79cec6c1566bd0e5c4
uzairrehan <uzairrehann@gmail.com>
2025-02-24 20:12:13 +0500

    cards section fully completd

---
+++ client/src/components/AllCards.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from './Card'
+
+function AllCards() {
+    return (
+        <>
+            <div className='w-full'>
+                <h1 className='text-center text-3xl md:text-5xl py-8 font-extrabold '>Our Loan Options</h1>
+                <div className="flex gap-2 flex-wrap bg-transparent justify-center items-center w-full ">
+                    <Card name={"Wedding Loan"} link={"/explore/wedding-loan"} desc={""} img={"/wedding.jpg"} />
+                    <Card name={"Business Loan"} link={"/explore/business-loan"} desc={""} img={"/business.jpg"} />
+                    <Card name={"Home Loan"} link={"/explore/home-loan"} desc={""} img={"/construction.jpeg"} />
+                    <Card name={"Education Loan"} link={"/explore/education-loan"} desc={""} img={"/education.jpg"} />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AllCards
--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,27 +1,35 @@
-import { CardTypes } from '@/types/cardType'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { FaArrowTrendUp } from 'react-icons/fa6'
-
-
+import { CardTypes } from '@/types/cardType';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { FaArrowTrendUp } from 'react-icons/fa6';
 
 function Card({ name, link, img, desc }: CardTypes) {
     return (
-        <div>
-            <Image src={img} alt={name} width={400} height={400} className='w-96 h-60 rounded-3xl' />
-            <div>
-                <h2>{name}</h2>
-                <Link href={link}>
-                    <button className="mt-4 text-sm text-left bg-white text-[#081b33] p-1 border-2 rounded-3xl  px-4 font-bold flex gap-2  items-center justify-center">
-                        Apply
-                        <FaArrowTrendUp />
-                    </button>
-                <p>{desc}</p>
-                </Link>
+        <>
+            <div className='rounded-lg bg-transparent flex justify-center items-center flex-col mx-3 relative'>
+                <div className="relative">
+                    <div className="absolute inset-0 bg-gradient-to-t from-[#071629]/100 z-10 transition-opacity duration-300 ease-in-out hover:opacity-0" />
+                    <Image
+                        src={img}
+                        alt={name}
+                        width={400}
+                        height={400}
+                        className="w-96 h-60 rounded-3xl shadow-md shadow-[#071629] border-2 transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-lg"
+                    />
+                </div>
+                <div className='bg-[#071629] px-2 border-2 z-30 rounded-2xl w-4/5 flex flex-row justify-center items-center gap-4 h-14 -translate-y-6 shadow-sm shadow-[#071629]'>
+                    <h2 className='font-bold text-sm md:text-md '>{name}</h2>
+                    <Link href={link}>
+                        <button className="text-sm text-left bg-white text-[#081b33] p-1 border-2 rounded-3xl  px-4 font-bold flex gap-2  items-center justify-center">
+                            More Info
+                            <FaArrowTrendUp />
+                        </button>
+                    </Link>
+                    <p className="text-sm text-gray-300">{desc}</p>
+                </div>
             </div>
-        </div>
-    )
+        </>
+    );
 }
-
-export default Card
+export default Card;

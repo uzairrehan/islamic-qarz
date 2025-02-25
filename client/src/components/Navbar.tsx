2d15cc1cd635d3bb4fd49a1ca59b664747bc3d44
uzairrehan <uzairrehann@gmail.com>
2025-02-25 13:50:15 +0500

    icons updated

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,9 +4,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { CiMenuBurger } from "react-icons/ci";
+import { FaArrowTrendUp, FaCircleInfo } from "react-icons/fa6";
 import { IoIosInformationCircleOutline } from "react-icons/io";
 import { IoLogInOutline } from "react-icons/io5";
 import { LuContactRound } from "react-icons/lu";
+import { MdEmail, MdOutlineEmail } from "react-icons/md";
 import { RxCross2 } from "react-icons/rx";
 import { SiWpexplorer } from "react-icons/si";
 
@@ -25,24 +27,27 @@ function Navbar() {
             height={80}
           />
           </Link>
-          <ul className="hidden items-center justify-between w-auto gap-4 text-md md:flex">
+          <ul className="hidden items-center justify-between w-auto gap-6 text-md md:flex">
             <Link href={"/explore"} className="flex gap-1 items-center justify-center">
-              <SiWpexplorer className="size-4" />
               <li>Explore</li>
+              <FaArrowTrendUp 
+              className="size-4" />
             </Link>
             <Link href={"/about"} className="flex gap-1 items-center justify-center">
-              <IoIosInformationCircleOutline className="size-5" />
               <li> About </li>
+              <FaCircleInfo 
+              className="size-4" />
             </Link>
             <Link href={"/contact"} className="flex gap-1 items-center justify-center">
-              <LuContactRound className="size-4" />
               <li>Contact</li>
+              <MdEmail
+              className="size-4" />
             </Link>
             <li>
               <Link href={"/login"}>
                 <button className="bg-white text-[#081b33] p-1 border-2 rounded-3xl ml-8 px-4 font-bold flex gap-2  items-center justify-center">
-                  <IoLogInOutline className="size-4" />
                   Login
+                  <IoLogInOutline className="size-4" />
                 </button>
               </Link>
             </li>
@@ -66,22 +71,27 @@ function Navbar() {
                 <ul className="items-center justify-around w-auto gap-4 text-md flex flex-col trans">
                   <RxCross2 onClick={() => { setOpen(!open) }} className="size-6 self-end" />
                   <Link href={"/explore"} className="flex gap-1 items-center justify-evenly w-4/5">
-                    <SiWpexplorer className="size-5" />
                     <li>Explore</li>
+                    
+              <FaArrowTrendUp 
+              className="size-4" />
                   </Link>
                   <Link href={"/about"} className="flex gap-1 items-center justify-evenly w-4/5">
-                    <IoIosInformationCircleOutline className="size-6" />
                     <li>&nbsp; About </li>
+                    
+              <FaCircleInfo 
+              className="size-4" />
                   </Link>
                   <Link href={"/contact"} className="flex gap-1 items-center justify-evenly w-4/5">
-                    <LuContactRound className="size-5" />
                     <li>Contact</li>
+                    <MdEmail
+              className="size-4" />
                   </Link>
                   <li>
                     <Link href={"/login"}>
                       <button className="bg-white text-[#081b33] p-2 border-2 rounded-3xl px-4 font-bold flex gap-4 items-center ">
-                        <IoLogInOutline className="size-5" />
                         Login
+                        <IoLogInOutline className="size-5" />
                       </button>
                     </Link>
                   </li>

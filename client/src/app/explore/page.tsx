36c20e29c4f8a864f7394052571e72c8464f4997
Syed Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-03-06 03:22:42 +0500

    explore section completed

--- client/src/app/explore/page.tsx
+++ client/src/app/explore/page.tsx
@@ -12,46 +12,138 @@ function Explore() {
           id="wedding"
           className="text-center text-2xl md:text-4xl py-12 font-extrabold"
         >
-          Wedding
+          Wedding Loans
         </div>
-        <div className="flex justify-center gap-5 md:gap-20 items-center flex-row w-full">
+        <div className="flex justify-center gap-4 md:gap-20 items-center flex-row w-full">
           <Image
             src={"/wedding.jpg"}
             alt="wedding"
             height={1000}
             width={1000}
-            className="md:w-[500px] w-60 rounded-xl"
+            className="md:w-[500px] md:h-[280px] w-[250px] h-[125px] rounded-xl"
           />
-          <div className="flex flex-col gap-1 md:gap-3">
+          <div className="flex flex-col gap-1 md:gap-3  md:w-full w-[126px]">
             <div>
-              <h1 className="text-xs md:text-xl font-extrabold">Subcategories: </h1>
-              <p className="text-xs font-light">
-                Valima, Furniture, Valima, Jahez
+              <h1 className="text-xs md:text-xl font-extrabold">
+                Subcategories:{" "}
+              </h1>
+              <p className="text-xs font-light w-full">
+                Valima, Furniture Jahez
               </p>
             </div>
             <div>
-              <h3 className="text-xs md:text-xl font-extrabold">Maximum loan: </h3>
+              <h3 className="text-xs md:text-xl font-extrabold">Max loan: </h3>
               <p className="text-xs font-light">PKR 5 Lakh</p>
             </div>
             <div>
-              <h3 className="text-xs md:text-xl font-extrabold">Loan period: </h3>
+              <h3 className="text-xs md:text-xl font-extrabold">
+                Loan period:{" "}
+              </h3>
               <p className="text-xs font-light">3 years</p>
             </div>
           </div>
         </div>
-
-        <div>
+        <div
+          id="home"
+          className="text-center text-2xl md:text-4xl py-12 font-extrabold"
+        >
+          Home Loans
+        </div>
+        <div className="flex justify-center gap-4 md:gap-20 items-center flex-row w-full">
           <Image
-            src={"/wedding.jpg"}
-            alt="wedding"
+            src={"/construction.jpeg"}
+            alt="construction"
             height={1000}
             width={1000}
-            className="md:w-[500px] w-64 rounded-xl "
+            className="md:w-[500px] md:h-[280px] w-[250px] h-[125px] rounded-xl"
           />
-          <div>
-            <h1>Subcategories: Valima, Furniture, Valima Food, Jahez</h1>
-            <h3>Maximum loan: PKR 5 Lakh</h3>
-            <h3>Loan period: 3 years</h3>
+          <div className="flex flex-col gap-1 md:gap-3  md:w-full w-[126px]">
+            <div>
+              <h1 className="text-xs md:text-xl font-extrabold">
+                Subcategories:{" "}
+              </h1>
+              <p className="text-xs font-light  w-full">Structure, Finishing</p>
+            </div>
+            <div>
+              <h3 className="text-xs md:text-xl font-extrabold">Max loan: </h3>
+              <p className="text-xs font-light">PKR 10 Lakh</p>
+            </div>
+            <div>
+              <h3 className="text-xs md:text-xl font-extrabold">
+                Loan period:{" "}
+              </h3>
+              <p className="text-xs font-light">5 years</p>
+            </div>
+          </div>
+        </div>
+        <div
+          id="business"
+          className="text-center text-2xl md:text-4xl py-12 font-extrabold"
+        >
+          Business Loans
+        </div>
+        <div className="flex justify-center gap-4 md:gap-20 items-center flex-row w-full">
+          <Image
+            src={"/business.jpg"}
+            alt="business"
+            height={1000}
+            width={1000}
+            className="md:w-[500px] md:h-[280px] w-[250px] h-[125px] rounded-xl"
+          />
+          <div className="flex flex-col gap-1 md:gap-3 md:w-full w-[126px]">
+            <div>
+              <h1 className="text-xs md:text-xl font-extrabold">
+                Subcategories:{" "}
+              </h1>
+              <p className="text-xs font-light  w-full">
+                Buy Stall, Advance Rent, Shop Assets
+              </p>
+            </div>
+            <div>
+              <h3 className="text-xs md:text-xl font-extrabold">Max loan: </h3>
+              <p className="text-xs font-light">PKR 10 Lakh</p>
+            </div>
+            <div>
+              <h3 className="text-xs md:text-xl font-extrabold">
+                Loan period:{" "}
+              </h3>
+              <p className="text-xs font-light">5 years</p>
+            </div>
+          </div>
+        </div>
+        <div
+          id="education"
+          className="text-center text-2xl md:text-4xl py-12 font-extrabold"
+        >
+          Education Loans
+        </div>
+        <div className="flex justify-center gap-4 md:gap-20 items-center flex-row w-full">
+          <Image
+            src={"/education.jpg"}
+            alt="education"
+            height={1000}
+            width={1000}
+            className="md:w-[500px] md:h-[280px] w-[250px] h-[125px] rounded-xl"
+          />
+          <div className="flex flex-col gap-1 md:gap-3 md:w-full w-[126px]">
+            <div>
+              <h1 className="text-xs md:text-xl font-extrabold">
+                Subcategories:{" "}
+              </h1>
+              <p className="text-xs font-light  w-full">
+                University Fees, Child Fees Loan
+              </p>
+            </div>
+            <div>
+              <h3 className="text-xs md:text-xl font-extrabold">Max loan: </h3>
+              <p className="text-xs font-light">PKR 5 Lakh</p>
+            </div>
+            <div>
+              <h3 className="text-xs md:text-xl font-extrabold">
+                Loan period:{" "}
+              </h3>
+              <p className="text-xs font-light">4 years</p>
+            </div>
           </div>
         </div>
       </div>

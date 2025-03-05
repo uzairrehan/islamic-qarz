83ce5801c1d110e53e44449d37de60e92aac5cac
Syed Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-03-06 03:33:00 +0500

    link update

--- client/src/components/AllCards.tsx
+++ client/src/components/AllCards.tsx
@@ -11,25 +11,25 @@ function AllCards() {
         <div className="flex gap-2 flex-wrap bg-transparent justify-center items-center w-full ">
           <Card
             name={"Wedding Loan"}
-            link={"/explore/wedding-loan"}
+            link={"/explore/#wedding"}
             desc={""}
             img={"/wedding.jpg"}
           />
           <Card
             name={"Business Loan"}
-            link={"/explore/business-loan"}
+            link={"/explore/#business"}
             desc={""}
             img={"/business.jpg"}
           />
           <Card
             name={"Home Loan"}
-            link={"/explore/home-loan"}
+            link={"/explore/#home"}
             desc={""}
             img={"/construction.jpeg"}
           />
           <Card
             name={"Education Loan"}
-            link={"/explore/education-loan"}
+            link={"/explore/#education"}
             desc={""}
             img={"/education.jpg"}
           />

6665e523c25bd999df2ebc9ecd32a8f63ecefdf2
Syed Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-03-07 12:27:24 +0500

    redux toolkit added

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import StoreProvider from "./StoreProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#071629]`}
       >
+        <StoreProvider>
         {children}
         <Footer />
+        </StoreProvider>
       </body>
     </html>
   );
